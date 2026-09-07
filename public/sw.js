fecea6cf859c5ee051ddbf00f547b371387ddf93
Clatlas <145463179+Clatlas@users.noreply.github.com>
2026-09-07 18:45:38 -0500

    Cache MRD remote update control

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mrd-v0.2.0-command-session';
-const APP_SHELL = ['/', '/index.html', '/styles.css?v=0.2.0', '/app.js?v=0.2.0', '/manifest.webmanifest', '/icon.svg'];
+const CACHE = 'mrd-v0.2.1-remote-update';
+const APP_SHELL = ['/', '/index.html', '/styles.css?v=0.2.0', '/app.js?v=0.2.0', '/update-control.js?v=0.2.1', '/manifest.webmanifest', '/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
